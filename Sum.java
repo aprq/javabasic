7ae074e044c74561330cc29de21e3325849d5b3a
Aprajita <89562532+aprq@users.noreply.github.com>
2022-12-11 20:36:19 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,11 @@
+public class Sum {
+    public static void main(String args[]) {
+        int a = 4;
+        int b = 5;
+        int c=a+b;
+        System.out.println("sum is " + c);
+
+        
+    }
+    
+}
